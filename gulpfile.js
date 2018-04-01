d79f1083d402bde625ce3b56ba1289c178062b3f
Ezra Chang <ezra.mintao.chang@gmail.com>
2018-03-31 22:34:57 -0500

    Incorporated less to make styling a more enjoyable activity.

--- gulpfile.js
+++ gulpfile.js
@@ -1,29 +1,29 @@
 const gulp = require('gulp');
-const uglify = require('gulp-uglify');
 const pump = require('pump');
+
 const cleanCSS = require('gulp-clean-css');
+const less = require('gulp-less');
+const uglify = require('gulp-uglify');
 
-const jsSrc = `${__dirname}/src/scripts`;
+const jsFiles = `${__dirname}/src/scripts/*.js`;
 const jsDest = `${__dirname}/static/scripts`;
-const jsFiles = `${jsSrc}/*.js`;
 
-const cssSrc = `${__dirname}/src/styles`;
+const lessFiles = `${__dirname}/src/styles/*.less`;
 const cssDest = `${__dirname}/static/styles`;
-const cssFiles = `${cssSrc}/*.css`;
 
 const minify = () => pump(
-  [ gulp.src(jsFiles), uglify(), gulp.dest(jsDest) ]
+	[ gulp.src(jsFiles), uglify(), gulp.dest(jsDest) ]
 );
 
 gulp.task('minify', minify);
 
 const shrink = () => pump(
-	[ gulp.src(cssFiles), cleanCSS(), gulp.dest(cssDest) ]
+	[ gulp.src(lessFiles), less(), cleanCSS(), gulp.dest(cssDest) ]
 );
 
 gulp.task('shrink', shrink);
 
 gulp.task('default', [ 'minify', 'shrink' ], () => {
 	gulp.watch(jsFiles, [ 'minify' ]);
-	gulp.watch(cssFiles, [ 'shrink' ]);
+	gulp.watch(lessFiles, [ 'shrink' ]);
 });
