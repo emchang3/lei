73112d356e64f4d548efe8bcaad7c90c39ec7668
Ezra Chang <ezra.mintao.chang@gmail.com>
2018-05-15 23:38:16 -0500

    refactor: Excludes _common.less from compilation.

--- gulpfile.js
+++ gulpfile.js
@@ -6,13 +6,14 @@ const cleanCSS = require('gulp-clean-css');
 const less = require('gulp-less');
 
 const lessFiles = `${__dirname}/src/styles/*.less`;
+const excludedLess = `!${__dirname}/src/styles/_*.less`;
 const cssDest = `${__dirname}/static/styles`;
 
 async function shrink() {
 	console.log('->\tCompiling and minifying .less');
 
 	await pump([
-		gulp.src(lessFiles),
+		gulp.src([ lessFiles, excludedLess ]),
 		less(),
 		cleanCSS(),
 		gulp.dest(cssDest)
